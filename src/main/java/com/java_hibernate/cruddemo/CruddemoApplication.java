d0db5ff5aef3abbf1077df5a969e14b375e356ef
Behnam <behnam.vosoogh@outlook.com>
2025-03-13 12:24:45 -0400

    Creat/Save added

--- src/main/java/com/java_hibernate/cruddemo/CruddemoApplication.java
+++ src/main/java/com/java_hibernate/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package com.java_hibernate.cruddemo;
 
+import com.java_hibernate.cruddemo.dao.StudentDao;
+import com.java_hibernate.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,10 +15,23 @@ public class CruddemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args){
+	public CommandLineRunner commandLineRunner(StudentDao studentDao){
 		return runner ->{
-			System.out.println("Hello World");
+			creteStudent(studentDao);
 		};
+	}
+
+	private void creteStudent(StudentDao studentDao) {
+		// create the student object
+		System.out.println("Creating new Student object ...");
+		Student tempStudnet = new Student("First Name0","Last Name 0","firstName1@email.com");
+
+		//save the student object
+		System.out.println("Saving the student ...");
+		studentDao.save(tempStudnet);
+
+		//display id of the saved student
+		System.out.println("Saved Student Generated id: ..."+tempStudnet.getId());
 	}
 
 }
---
+++ src/main/java/com/java_hibernate/cruddemo/dao/StudentDao.java
@@ -0,0 +1,8 @@
+package com.java_hibernate.cruddemo.dao;
+
+import com.java_hibernate.cruddemo.entity.Student;
+
+public interface StudentDao {
+
+    void save(Student theStudent);
+}
